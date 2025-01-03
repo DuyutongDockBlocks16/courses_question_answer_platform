01d3d36b967ca7b7b58062962bb8f7d427b2ad64
YutongDu <yutong.du@aalto.fi>
2025-01-03 22:59:56 +0200

    add tailwind.config.ts

---
+++ qa-ui/tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss';
+import flowbitePlugin from 'flowbite/plugin'
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite-svelte/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
+  theme: {
+    extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B'
+        },
+        blue: {
+          50:"#eff6ff",100:"#dbeafe",200:"#bfdbfe",300:"#93c5fd",400:"#60a5fa",500:"#3b82f6",600:"#2563eb",700:"#1d4ed8",800:"#1e40af",900:"#1e3a8a"
+        },
+      }
+    }
+  },
+  plugins: [flowbitePlugin]
+} as Config;
+
+
+
