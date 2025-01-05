9a3a090f948a07c397f80868ecfa731eaec9c881
YutongDu <yutong.du@aalto.fi>
2025-01-05 20:33:30 +0200

    modify k6

--- k6/performance-test-post-answer.js
+++ k6/performance-test-post-answer.js
@@ -10,8 +10,8 @@ export default function () {
     http.post(
       "http://localhost:7800/api/questions/1/answers",
       JSON.stringify({
-        user_uuid: `user-${Math.floor(Math.random() * 100000000000)}`,
-        content: `answer-${Math.floor(Math.random() * 1000)}`,
+        user: `user-${Math.floor(Math.random() * 100000000000)}`,
+        answer_content: `answer-${Math.floor(Math.random() * 1000)}`,
       }),
     );
 }
@@ -19,6 +19,7 @@ export default function () {
   // k6 run performance-test-post-answer.js
 
   
+
 //   /\      Grafana   /‾‾/  
 //   /\  /  \     |\  __   /  /   
 //  /  \/    \    | |/ /  /   ‾‾\ 
@@ -33,22 +34,23 @@ export default function () {
 //             * default: 10 looping VUs for 5s (gracefulStop: 30s)
 
 
-//    data_received..............: 8.6 MB  1.7 MB/s
-//    data_sent..................: 1.3 MB  258 kB/s
-//    http_req_blocked...........: med=2.75µs  p(99)=6.91µs  
-//    http_req_connecting........: med=0s      p(99)=0s      
-//    http_req_duration..........: med=6.45ms  p(99)=13.13ms 
-//    http_req_failed............: 100.00% 7207 out of 7207
-//    http_req_receiving.........: med=61.68µs p(99)=131.31µs
-//    http_req_sending...........: med=10.96µs p(99)=41.63µs 
-//    http_req_tls_handshaking...: med=0s      p(99)=0s      
-//    http_req_waiting...........: med=6.37ms  p(99)=13ms    
-//    http_reqs..................: 7207    1440.17056/s
-//    iteration_duration.........: med=6.53ms  p(99)=13.22ms 
-//    iterations.................: 7207    1440.17056/s
-//    vus........................: 10      min=10           max=10
-//    vus_max....................: 10      min=10           max=10
+//    data_received..................: 1.3 MB 263 kB/s
+//    data_sent......................: 1.2 MB 246 kB/s
+//    http_req_blocked...............: med=3.15µs  p(99)=7.14µs  
+//    http_req_connecting............: med=0s      p(99)=0s      
+//    http_req_duration..............: med=7.09ms  p(99)=12.41ms 
+//      { expected_response:true }...: med=7.09ms  p(99)=12.41ms 
+//    http_req_failed................: 0.00%  0 out of 6800
+//    http_req_receiving.............: med=47.95µs p(99)=126.07µs
+//    http_req_sending...............: med=12.6µs  p(99)=44.66µs 
+//    http_req_tls_handshaking.......: med=0s      p(99)=0s      
+//    http_req_waiting...............: med=7.02ms  p(99)=12.35ms 
+//    http_reqs......................: 6800   1358.387107/s
+//    iteration_duration.............: med=7.17ms  p(99)=12.5ms  
+//    iterations.....................: 6800   1358.387107/s
+//    vus............................: 10     min=10        max=10
+//    vus_max........................: 10     min=10        max=10
 
 
-// running (05.0s), 00/10 VUs, 7207 complete and 0 interrupted iterations
+// running (05.0s), 00/10 VUs, 6800 complete and 0 interrupted iterations
 // default ✓ [======================================] 10 VUs  5s
--- k6/performance-test-post-question.js
+++ k6/performance-test-post-question.js
@@ -10,8 +10,9 @@ export default function () {
     http.post(
       "http://localhost:7800/api/courses/1/questions",
       JSON.stringify({
-        user_uuid: `user-${Math.floor(Math.random() * 100000000000)}`,
-        content: `question-${Math.floor(Math.random() * 1000)}`,
+        user: `user-${Math.floor(Math.random() * 100000000000)}`,
+        question_title: `question-${Math.floor(Math.random() * 1000)}`,
+        question_id: 1,
       }),
     );
 }
@@ -21,6 +22,7 @@ export default function () {
 
 
   
+
 //   /\      Grafana   /‾‾/  
 //   /\  /  \     |\  __   /  /   
 //  /  \/    \    | |/ /  /   ‾‾\ 
@@ -35,22 +37,23 @@ export default function () {
 //             * default: 10 looping VUs for 5s (gracefulStop: 30s)
 
 
-//    data_received..............: 8.7 MB  1.7 MB/s
-//    data_sent..................: 1.3 MB  264 kB/s
-//    http_req_blocked...........: med=2.73µs  p(99)=7.41µs  
-//    http_req_connecting........: med=0s      p(99)=0s      
-//    http_req_duration..........: med=6.1ms   p(99)=14.29ms 
-//    http_req_failed............: 100.00% 7298 out of 7298
-//    http_req_receiving.........: med=59.69µs p(99)=130.21µs
-//    http_req_sending...........: med=10.46µs p(99)=43.66µs 
-//    http_req_tls_handshaking...: med=0s      p(99)=0s      
-//    http_req_waiting...........: med=6.02ms  p(99)=14.19ms 
-//    http_reqs..................: 7298    1457.742005/s
-//    iteration_duration.........: med=6.19ms  p(99)=14.42ms 
-//    iterations.................: 7298    1457.742005/s
-//    vus........................: 10      min=10           max=10
-//    vus_max....................: 10      min=10           max=10
+//    data_received..................: 1.2 MB 232 kB/s
+//    data_sent......................: 1.2 MB 236 kB/s
+//    http_req_blocked...............: med=3.37µs  p(99)=8.49µs  
+//    http_req_connecting............: med=0s      p(99)=0s      
+//    http_req_duration..............: med=7.62ms  p(99)=13.95ms 
+//      { expected_response:true }...: med=7.62ms  p(99)=13.95ms 
+//    http_req_failed................: 0.00%  0 out of 5931
+//    http_req_receiving.............: med=52.55µs p(99)=125.72µs
+//    http_req_sending...............: med=13.16µs p(99)=54.94µs 
+//    http_req_tls_handshaking.......: med=0s      p(99)=0s      
+//    http_req_waiting...............: med=7.54ms  p(99)=13.87ms 
+//    http_reqs......................: 5931   1184.934409/s
+//    iteration_duration.............: med=7.72ms  p(99)=14.05ms 
+//    iterations.....................: 5931   1184.934409/s
+//    vus............................: 10     min=10        max=10
+//    vus_max........................: 10     min=10        max=10
 
 
-// running (05.0s), 00/10 VUs, 7298 complete and 0 interrupted iterations
+// running (05.0s), 00/10 VUs, 5931 complete and 0 interrupted iterations
 // default ✓ [======================================] 10 VUs  5s
