0326cce3f4218e63df95a9526040b91729da3661
YutongDu <yutong.du@aalto.fi>
2025-03-05 16:59:01 +0200

    add reflection.md

--- qa-ui/src/stores/stores.js
+++ qa-ui/src/stores/stores.js
@@ -2,8 +2,16 @@ import { readable } from "svelte/store";
 
 let user = localStorage.getItem("userUuid");
 
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 if (!user) {
-  user = crypto.randomUUID().toString();
+  user = generateUUID();
   localStorage.setItem("userUuid", user);
 } 
 
